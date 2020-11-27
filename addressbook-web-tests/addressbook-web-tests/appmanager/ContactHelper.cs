4a0df346a5fb2b4c804bcd4e8d2275ef1b3b2bd9
Vadym Marochok <marochok@gmail.com>
2020-11-27 23:53:46 +0200

    ContactHelper class is created

---
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    public class ContactHelper : HelperBase
+    {
+        public ContactHelper(ApplicationManager manager)
+            : base(manager)
+        {
+        }
+
+        public ContactHelper Create(ContactData contact)
+        {
+            manager.Navigator.GoToAddNewPage();
+            FillContactForm(contact);
+            SubmitContactCreation();
+            ReturnToHomePage();
+            return this;
+        }
+        public void FillContactForm(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+        }
+
+        public void SubmitContactCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        public void ReturnToHomePage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+
+    }
+}
