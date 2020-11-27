c04bbee977fd6059ef7655d3a9dda39bf02951f2
Vadym Marochok <marochok@gmail.com>
2020-11-27 23:51:32 +0200

    Contact creation file is updated

---
+++ addressbook-web-tests/addressbook-web-tests/tests/ContactCreationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests : TestBase
+    {
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            ContactData contact = new ContactData("Bob");
+            contact.Lastname = "Smith";
+            app.Contacts.Create(contact);
+            app.Navigator.Logout();
+        }
+    }
+}
